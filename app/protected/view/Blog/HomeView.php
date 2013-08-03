f977f3a4582781e672ae09b3a86df4d5d87b26ae
Swapnil Patil <newuser@example.com>
2013-08-03 18:39:07 +0530

    fixed issue

--- app/protected/view/Blog/HomeView.php
+++ app/protected/view/Blog/HomeView.php
@@ -30,7 +30,6 @@
         <input type="text" name='title'>
         <input type="submit" name="submit" value="search">
     </form>
-<<<<<<< HEAD
 <table border="1" class='table'>
     <?php
     if(Application::session()->read('userId')!==null){
@@ -62,24 +61,5 @@
             ?>
         </td></tr>
 </table>
-=======
-    <table border="1" class='table'>
-        <?php
-        foreach($this->blogs as $blog){
-        if($blog['isApprove']=='Yes'||$this->userRole===1){
-        ?>
-        <tr><td><table border="1" style="width: 250px;">
-                    <tr><td>Title :</td><td><?php echo $blog['title'];?></td></tr>
-                    <tr><td>Description:</td><td><?php echo substr(html_entity_decode($blog['description']),0,50).'...';?></td></tr>
-                    <tr><td colspan=2 align='center'><a href=<?php echo '/specific/'.$blog['id']; ?>>read more..</a></td></tr>
-                </table>
-                <?php
-
-                }
-                }
-                ?>
-            </td></tr>
-    </table>
->>>>>>> 692978ffd4d6a84c8f6811d908eda5e21597cc07
 </body>
 </html>
